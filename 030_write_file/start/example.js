3434bc125f4895f163290c812ca5b14976be7368
morihisashi <hisa.1995.322@outlook.com>
2023-03-18 12:55:51 +0900

    __dirnameと__filenameの使い方について

--- 030_write_file/start/example.js
+++ 030_write_file/start/example.js
@@ -1,1 +1,6 @@
-console.log('hello, node.js');
+const fs = require('fs');
+
+console.log(__filename);
+
+fs.writeFileSync(__dirname + '/test.txt', 'hello, node,js');
+//console.log('hello, node.js');
