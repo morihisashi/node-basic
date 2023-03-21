8c1012a207360ccd899970ed37db5b02ca665f6b
morihisashi <hisa.1995.322@outlook.com>
2023-03-21 09:44:55 +0900

    CJSとESMの挙動の違いについて学ぼう

---
+++ 070_esm_vs_cjs/test.mjs
@@ -0,0 +1,17 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// console.log(__filename);
+// console.log(__dirname);
+
+// const __dirname2 = fileURLToPath(new URL('.', import.meta.url));
+// console.log(__dirname2);
+// console.log(new URL('.', import.meta.url));
+
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url)
+// const jsonObj = require('./sample.json');
+// console.log(require);
